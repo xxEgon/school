5bef264ed7ceada614467ccc7b9d6bac1a56e3ad
Damian Filipowski <47593162+xxEgon@users.noreply.github.com>
2019-02-13 12:03:07 +0100

    Add files via upload

---
+++ JS/3.Lotto/static/ajax2.php
@@ -0,0 +1,85 @@
+<?php
+	header('Content-Type: text/plain');
+	
+	include("hasla2.php"); //require
+	
+	try {
+
+		$dbh = new PDO($dsn, $user, $password);		
+
+	} catch (PDOException $e) {
+
+		echo 'Connection failed: ' . $e->getMessage();
+	}
+	
+	if(isset($_POST['f']) && $_POST['f']==1) {
+
+		global $wynik;
+		
+		$wynik= "";
+
+		function AddRandomNumberToString($w) {
+			
+			do {
+
+				$liczba = (string)mt_rand(1,49);
+
+			} while ( strstr( $w , $liczba) != false ) ;
+
+			$w.= ($liczba.";") ;
+			return $w;
+		}
+		
+		for( $i=0 ; $i < 6 ; $i++ )
+		{	
+			$wynik = AddRandomNumberToString($wynik);			
+		}
+		
+		$wynik = substr($wynik, 0, -1);
+	
+		$data= date("d-m-Y H:i:s");
+		
+		$sth = $dbh->prepare('insert into lotto(wynik,data) values(:wynik,:data)');
+		$sth->bindValue(':wynik', $wynik, PDO::PARAM_STR);
+		$sth->bindValue(':data', $data, PDO::PARAM_STR);
+		$sth->execute();
+
+		if(isset($_POST['f']) && $_POST['f']==1 && isset($_POST['s']) && $_POST['s']=="p") {
+
+
+			$sth = $dbh->prepare('select * from lotto');
+			$sth->execute();
+			$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+			$arr=Array(
+				"dane" => $result,
+				"ok" => 1
+			);
+			echo json_encode($arr);
+
+		}
+		elseif(isset($_POST['s']) && $_POST['s']=="k") {
+		
+			$sth = $dbh->prepare('select * from lotto order by id desc limit 1');
+			$sth->execute();
+			$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+			$arr=Array(
+				"dane" => $result,
+				"ok" => 1
+			);
+			echo json_encode($arr);
+			
+		}
+		
+	}elseif(isset($_POST['f']) && $_POST['f']==2) {
+		
+		$sth = $dbh->prepare('delete from lotto WHERE lotto.id ='. $_POST['s']);
+		$sth->execute();
+		$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+		$arr=Array(
+			"dane" => $result,
+			"ok" => 1
+		);
+		echo json_encode($arr);
+	}
+	
+?>
---
+++ JS/4.OX/static/ajax.php
@@ -0,0 +1,174 @@
+<?php
+	header('Content-Type: text/plain');
+
+	include("hasla.php"); 
+
+	try {
+		$dbh = new PDO($dsn, $user, $password);	
+		//$dbh = new PDO($dsn, $user);		
+	} catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+	}
+
+	if(isset($_POST['a']) ) {		
+		
+		$a = $_POST['a'];
+
+		switch($a) 
+		{
+			case -3:
+				$b = $_POST['b'];
+
+				$sth = $dbh->prepare('update ox set user = :user');
+				$sth->bindValue(':user', $b, PDO::PARAM_STR);
+				$sth->execute();
+				$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+				$sth = $dbh->prepare('update ox set updated = :updated');
+				$sth->bindValue(':updated', "-1", PDO::PARAM_STR);
+				$sth->execute();
+
+				$sth = $dbh->prepare('update ox set end = :end');
+				$sth->bindValue(':end', "false", PDO::PARAM_STR);
+				$sth->execute();
+
+				$res=Array(
+					"ok" => 1,
+					"action" => "userUpdated",
+				);
+
+				echo json_encode($res);
+				break;
+			case -2:
+
+				$sth = $dbh->prepare("select user from ox ");
+				$sth->execute();
+				$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+				$result = $result[0]['user'];
+
+				if($result != "x" && $result != "o")
+				{
+					$res=Array(
+						"ok" => 1,
+						"action" => "notChoosed",
+					);
+				}
+				else 
+				{
+
+					if($result == "x")
+						$u = "o";
+					else 
+						$u = "x";
+
+					$res=Array(
+						"ok" => 1,
+						"action" => "Choosed",
+						"user" => $u,
+					);
+
+				}				
+
+				echo json_encode($res);
+				break;
+			case -1:
+				$sth = $dbh->prepare("select updated from ox ");
+				$sth->execute();
+				$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+				$result = $result[0]['updated'];
+
+				if($result == "-1" )
+				{
+					$res=Array(
+						"ok" => 1,
+						"action" => "notUpdated",
+					);
+				}
+				else 
+				{					
+					$res=Array(
+						"ok" => 1,
+						"action" => "Updated",
+						"index" => $result,
+					);
+					$sth = $dbh->prepare('update ox set updated = :updated');
+					$sth->bindValue(':updated', "-1", PDO::PARAM_STR);
+					$sth->execute();
+				}				
+
+				echo json_encode($res);
+				break;
+			case 1:
+				$b = $_POST['b'];
+
+				$sth = $dbh->prepare('update ox set updated = :updated');
+				$sth->bindValue(':updated', $b, PDO::PARAM_STR);
+				$sth->execute();
+
+				$res=Array(
+					"ok" => 1,
+					"action" => "updateSuccess",
+				);
+
+				echo json_encode($res);
+				break;
+			case 2:
+				
+				$sth = $dbh->prepare("select end from ox ");
+				$sth->execute();
+				$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+				$result = $result[0]['end'];
+
+				
+				if($result == "true") {
+					$res=Array(
+						"ok" => 1,
+						"action" => "End",
+					);
+				}
+				else {
+					if($result == "draw") {
+						$res=Array(
+							"ok" => 1,
+							"action" => "Draw",
+						);
+					}
+					else {
+						$res=Array(
+							"ok" => 1,
+							"action" => "notEnd",
+						);
+					}
+				}
+				echo json_encode($res);
+				break;
+			case 3:
+				$b = $_POST['b'];
+
+				if($b=="win") {
+					$sth = $dbh->prepare('update ox set end = :end');
+					$sth->bindValue(':end', "true", PDO::PARAM_STR);
+					$sth->execute();
+
+					$res=Array(
+						"ok" => 1,
+						"action" => "I_won",
+					);
+				}
+				else {
+					$sth = $dbh->prepare('update ox set end = :end');
+					$sth->bindValue(':end', "draw", PDO::PARAM_STR);
+					$sth->execute();
+
+					$res=Array(
+						"ok" => 1,
+						"action" => "Draw",
+					);
+				}
+				echo json_encode($res);
+				break;
+		}
+	}
+	
+?>
